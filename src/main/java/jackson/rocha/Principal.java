8492b35d4323e47eccd9c8baed6ab9673a692a5e
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-20 19:28:12 -0300

    Projeto Interfaces

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -1,15 +1,22 @@
 package jackson.rocha;
 
+import jackson.rocha.contaspagar.modelo.Holerite;
+import jackson.rocha.contaspagar.modelo.OrdemServico;
 import jackson.rocha.contaspagar.servico.ServicoContaPagar;
-import jackson.rocha.pagamento.DocumentoPagavel;
+import jackson.rocha.pagamento.Beneficiario;
 
 public class Principal {
     public static void main(String[] args) {
 
         ServicoContaPagar servicoContaPagar = new ServicoContaPagar();
-        
-        DocumentoPagavel documento = null;
 
-        servicoContaPagar.pagar(documento);
+        Beneficiario funcionario = new Beneficiario("Jackson", "014222222160", "123456");
+        Holerite holerite = new Holerite(funcionario, 100, 168);
+
+        Beneficiario fornecedor = new Beneficiario("Consultoria XYZ", "119086282639829", "23456789");
+        OrdemServico os = new OrdemServico(fornecedor, 65_500);
+
+        servicoContaPagar.pagar(holerite);
+        servicoContaPagar.pagar(os);
     }
 }
