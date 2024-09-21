693f155d4cd4f0c7fb5e36724281a23722f9cb49
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-21 07:24:40 -0300

    Usando a injeção de dependências

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -4,11 +4,13 @@ import jackson.rocha.contaspagar.modelo.Holerite;
 import jackson.rocha.contaspagar.modelo.OrdemServico;
 import jackson.rocha.contaspagar.servico.ServicoContaPagar;
 import jackson.rocha.pagamento.Beneficiario;
+import jackson.rocha.pagamento.MetodoPagamento;
+import jackson.rocha.pagamento.Transferencia;
 
 public class Principal {
     public static void main(String[] args) {
-
-        ServicoContaPagar servicoContaPagar = new ServicoContaPagar();
+        MetodoPagamento metodoPagamento = new Transferencia();
+        ServicoContaPagar servicoContaPagar = new ServicoContaPagar(metodoPagamento);
 
         Beneficiario funcionario = new Beneficiario("Jackson", "014222222160", "123456");
         Holerite holerite = new Holerite(funcionario, 100, 168);
